d52e9fd64798648c248242f5c4526437447bed2a
jacksonjunelee <jjlee5266@gmail.com>
2017-01-24 17:18:04 -0500

    moved credentials to environment variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,9 @@ class SessionsController < ApplicationController
     username = params[:username]
     password = params[:password]
 
-    user = HTTParty.post("https://login.zype.com/oauth/token/?client_id=61255480307354ebd4d094482f2483adec9942637979aa5c3963ecbac469f943&client_secret=926e632b2aa9758f60dbdf2f8de13bebd9a04dfd602de4c257b3f8b4a97cf0b8&username=#{username}&password=#{password}&grant_type=password")
+    user = HTTParty.post("https://login.zype.com/oauth/token/?client_id=#{ENV['zype_client_id']}&client_secret=#{ENV['zype_client_secret']}&username=#{username}&password=#{password}&grant_type=password")
+
+    binding.pry
 
     if !user['error']
       session[:current_user_access_token] = user['access_token']
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,12 +1,12 @@
 class VideosController < ApplicationController
 
   def index
-    @videos = HTTParty.get('https://api.zype.com/videos?app_key=XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf')['response']
+    @videos = HTTParty.get("https://api.zype.com/videos?app_key=#{ENV['zype_app_key']}")['response']
   end
 
   def show
     id = params['id']
-    @video = HTTParty.get("https://api.zype.com/videos/#{id}?app_key=XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf")['response']
+    @video = HTTParty.get("https://api.zype.com/videos/#{id}?app_key=#{ENV['zype_app_key']}")['response']
   end
 
 end
